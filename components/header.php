4c39a30c67043dbc5be0c97c83bcb7cc62aa44f1
salahxgamer <salahxgamer2015@gmail.com>
2022-06-05 16:17:02 +0100

    Fixed header to show correct pages

--- components/header.php
+++ components/header.php
@@ -3,7 +3,7 @@
         <div class="container">
             <a class="navbar-brand d-flex align-items-center" href="index.php">
                 <span class="bs-icon-sm bs-icon-rounded bs-icon-primary d-flex justify-content-center align-items-center me-2 bs-icon">
-                    <img src="img/logo/logo.png" height="40" alt="">
+                    <img src="assets/img/logo/logo.png" height="40" alt="">
                 </span>
                 <span>E-Learning</span>
             </a>
@@ -13,19 +13,26 @@
             </button>
             <div id="navcol-2" class="collapse navbar-collapse">
                 <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
+                    <li class="nav-item"><a class="nav-link" href="tutorials.php">Tutorials</a></li>
+                    <li class="nav-item"><a class="nav-link" href="testimonials.php">Testimonials</a></li>
+                    <li class="nav-item"><a class="nav-link" href="blog.php">Blog</a></li>
+                    <li class="nav-item"><a class="nav-link" href="about.php">About</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
                     <?php if ($user->is_logged()) : ?>
-                        <li class="nav-item"><a class="nav-link" href="index.php">Home</a></li>
-                        <li class="nav-item"><a class="nav-link" href="categories.php">Category</a></li>
-                        <li class="nav-item"><a class="nav-link" href="article.php">Article</a></li>
-                        <li class="nav-item"><a class="nav-link" href="author.php">Author</a></li>
+                        <li class="nav-item"><a class="nav-link" href="profile.php">Profile</a></li>
+                        <li class="nav-item"><a class="nav-link" href="dashboard.php">Dashboard</a></li>
+                        <a class="btn btn-primary ms-lg-2" role="button" href="Logout.php">
+                            <i class="fas fa-sign-out"></i>
+                            Logout
+                        </a>
                     <?php else : ?>
-                        <li class="nav-item"><a class="nav-link" href="articleOfCategory.php">Articles</a></li>
-                        <li class="nav-item"><a class="nav-link" href="contact.php">Contact</a></li>
+                        <a class="btn btn-primary ms-lg-2" role="button" href="login.php">
+                            <i class="fas fa-sign-in"></i>
+                            Sign in
+                        </a>
                     <?php endif;  ?>
                 </ul>
-                <a class="btn btn-primary ms-lg-2" role="button" href="<?= ($user->is_logged()) ? 'Logout.php' : 'login.php'; ?>">
-                    <?= ($user->is_logged()) ? 'Logout' : 'Sign in'; ?>
-                </a>
             </div>
         </div>
     </nav>
