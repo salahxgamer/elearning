1b4ef411af2b8640ac3ab924fa0c6370936a2568
salahxgamer <salahxgamer2015@gmail.com>
2022-06-05 16:05:05 +0100

    Fixed logo Removed unused link Added logo file

--- components/head.php
+++ components/head.php
@@ -9,7 +9,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
 
 	<title>E-Learning Tutor</title>
-	<link rel="icon" href="img/logo/logo.png" sizes="32x32" type="image/png">
+	<link rel="icon" href="assets/img/logo/logo.png" sizes="32x32" type="image/png">
 
 	<!-- Bootstrap -->
 	<!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" crossorigin="anonymous"> -->
@@ -17,9 +17,6 @@
 
 	<!-- FontAwesome -->
 	<link rel="stylesheet" href="assets/fonts/fontawesome-all.min.css">
-
-	<!-- Custom Styling-->
-	<link rel="stylesheet" href="css/footer.css">
 
 	<!-- Fonts -->
 	<link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i&amp;display=swap">
