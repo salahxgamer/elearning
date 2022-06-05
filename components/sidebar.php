2c87483dc5b82c5d4f137fe8fbf3903c25f71ef6
salahxgamer <salahxgamer2015@gmail.com>
2022-06-05 15:49:30 +0100

    Added sidebar

---
+++ components/sidebar.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-dark align-items-start sidebar sidebar-dark accordion bg-gradient-primary p-0">
+    <div class="container-fluid d-flex flex-column p-0">
+        <a class="navbar-brand d-flex justify-content-center align-items-center sidebar-brand m-0" href="#">
+            <div class="sidebar-brand-icon rotate-n-15">
+                <img src="img/logo/logo.png" height="40" alt="">
+            </div>
+            <div class="sidebar-brand-text mx-3">
+                <span>E-Learning</span>
+            </div>
+        </a>
+        <hr class="sidebar-divider my-0">
+        <ul class="navbar-nav text-light" id="accordionSidebar">
+            <li class="nav-item">
+                <a class="nav-link" href="dashboard.php">
+                    <i class="fas fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="profile.php">
+                    <i class="fas fa-user"></i>
+                    <span>Profile</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="table.php">
+                    <i class="fas fa-table"></i>
+                    <span>Table</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="login.php">
+                    <i class="far fa-user-circle"></i>
+                    <span>Login</span>
+                </a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="logout.php">
+                    <i class="fas fa-sign-out"></i>
+                    <span>Logout</span>
+                </a>
+            </li>
+        </ul>
+        <div class="text-center d-none d-md-inline">
+            <button class="btn rounded-circle border-0" id="sidebarToggle" type="button">
+            </button>
+        </div>
+    </div>
+</nav>
