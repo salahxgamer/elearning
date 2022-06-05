6cd04b66fc3765a0eac0b1f152f0db83a0664680
salahxgamer <salahxgamer2015@gmail.com>
2022-06-05 15:49:11 +0100

    Added footer

---
+++ components/footer.php
@@ -0,0 +1,35 @@
+<footer class="footer blog-footer">
+        <div class="container">
+                <ul class="list-inline mb-3 h5">
+                        <li class="list-inline-item"><a class="text-decoration-none" href="#">Article</a></li>
+                        <li class="list-inline-item"><a class="text-decoration-none" href="#">License Terms and Conditions</a></li>
+                        <li class="list-inline-item"><a class="text-decoration-none" href="#">Privacy Policy</a></li>
+                        <li class="list-inline-item"><a class="text-decoration-none" href="contact.php">Contact</a></li>
+                </ul>
+                <p class="text-muted">Copyright © <a class="text-decoration-none" href="https://github.com/salahxgamer">@SXG</a>. All Rights Reserved.</p>
+                <ul class="list-inline mt-2">
+                        <li class="list-inline-item">
+                                <a href="https://www.instagram.com/salah_e.a" target="_blanc">
+                                        <i class="fa fa-lg fa-instagram" aria-hidden="true"></i>
+                                </a>
+                        </li>
+                        <li class="list-inline-item">
+                                <a href="https://twitter.com/Salah_XG" target="_blanc">
+                                        <i class="fa fa-lg fa-twitter" aria-hidden="true"></i>
+                                </a>
+                        </li>
+                        <li class="list-inline-item">
+                                <a href="https://github.com/salahxgamer" target="_blanc">
+                                        <i class="fa fa-lg fa-github" aria-hidden="true"></i>
+                                </a>
+                        </li>
+                </ul>
+        </div>
+        <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+        <script src="assets/bootstrap/js/bootstrap.min.js" type="text/javascript" charset="utf-8" async defer></script>
+        <script src="assets/js/bs-init.js"></script>
+        <script src="assets/js/theme.js"></script>
+        <script src="https://kit.fontawesome.com/c10842439c.js" crossorigin="anonymous"></script>
+</footer>
